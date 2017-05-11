2f919f0255851fe066cf0fa874419d417f3a2d9d
Carlin Yuen <email.me@carlinyuen.com>
2017-05-11 00:57:25 -0400

    tried to use jquery everywhere, broke something

--- app/scripts/newtab.js
+++ app/scripts/newtab.js
@@ -6,27 +6,28 @@ var googlePlusUserLoader = (function() {
   var STATE_ACQUIRING_AUTHTOKEN=2;
   var STATE_AUTHTOKEN_ACQUIRED=3;
   var state = STATE_START;
+  var buttonSignin, labelWelcome;
 
   function disableButton(button) {
-    button.setAttribute('disabled', 'disabled');
+    button.attr('disabled', 'disabled');
   }
 
   function enableButton(button) {
-    button.removeAttribute('disabled');
+    button.attr('disabled');
   }
 
   function changeState(newState) {
     state = newState;
     switch (state) {
       case STATE_START:
-        enableButton(signin_button);
+        enableButton(buttonSignin);
         break;
       case STATE_ACQUIRING_AUTHTOKEN:
-        sampleSupport.log('Acquiring token...');
-        disableButton($('button.signin'));
+        console.log('Acquiring token...');
+        disableButton(buttonSignin);
         break;
       case STATE_AUTHTOKEN_ACQUIRED:
-        $('button.signin').fadeOut(function() {this.remove();});
+        buttonSignin.fadeOut(function() {this.remove();});
         break;
     }
   }
@@ -39,101 +40,111 @@ var googlePlusUserLoader = (function() {
     getToken();
 
     function getToken() {
-      chrome.identity.getAuthToken({ interactive: interactive }, function(token) {
+      chrome.identity.getAuthToken({
+        interactive: interactive
+      }, function(token) {
         if (chrome.runtime.lastError) {
           callback(chrome.runtime.lastError);
           return;
         }
 
+        console.log(token);
         access_token = token;
         requestStart();
       });
     }
 
     function requestStart() {
-      var xhr = new XMLHttpRequest();
-      xhr.open(method, url);
-      xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-      xhr.onload = requestComplete;
-      xhr.send();
+      $.ajax({
+        type: method,
+        url: url,
+        headers: {
+          "Authorization": 'Bearer ' + access_token
+        }
+      }, function(data) {callback(data);})
+        .fail(function() {
+          console.log("FAIL");
+          retry = false;
+          chrome.identity.removeCachedAuthToken({
+            token: access_token
+          }, getToken)
+        });
+      // var xhr = new XMLHttpRequest();
+      // console.log(access_token);
+      // xhr.open(method, url);
+      // xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+      // xhr.onload = requestComplete;
+      // xhr.send();
     }
 
-    function requestComplete() {
-      if (this.status == 401 && retry) {
-        retry = false;
-        chrome.identity.removeCachedAuthToken({ token: access_token },
-                                              getToken);
-      } else {
-        callback(null, this.status, this.response);
-      }
-    }
+    // function requestComplete() {
+      // if (this.status == 401 && retry) {
+        // retry = false;
+        // chrome.identity.removeCachedAuthToken({
+        //   token: access_token
+        // }, getToken);
+      // } else {
+        // callback(null, this.status, this.response);
+      // }
+    // }
   }
 
-  function getEvents() {
+  function getEvents(interactive) {
+    console.log('getUserInfo:', interactive);
     requestWithAuth('GET',
-                'https://www.googleapis.com/calendar/v3/calendars/primary/events',
-                false,
-                onEventsFetched)
+      'https://www.googleapis.com/calendar/v3/calendars/primary/events',
+      interactive,
+      onEventsFetched)
+  }
+
+  function onEventsFetched(data) {
+    console.log('eventsFetched:', data);
+
   }
 
   function getUserInfo(interactive) {
+    console.log('getUserInfo:', interactive);
     requestWithAuth('GET',
-                'https://www.googleapis.com/plus/v1/people/me',
-                interactive,
-                onUserInfoFetched);
+      'https://www.googleapis.com/plus/v1/people/me',
+      interactive,
+      onUserInfoFetched);
   }
 
   // Code updating the user interface, when the user information has been
   // fetched or displaying the error.
-  function onUserInfoFetched(error, status, response) {
-    if (!error && status == 200) {
+  function onUserInfoFetched(data) {
+    console.log('UserInfoFetched:', data);
+    if (data) {
       changeState(STATE_AUTHTOKEN_ACQUIRED);
-      sampleSupport.log(response);
-      var user_info = JSON.parse(response);
-      populateUserInfo(user_info);
+      populateUserInfo(data);
     } else {
       changeState(STATE_START);
     }
   }
 
   function populateUserInfo(user_info) {
-    labelWelcome.innerHTML = 'Hello ' + user_info.displayName;
-    fetchImageBytes(user_info);
-  }
-
-  function fetchImageBytes(user_info) {
-    if (!user_info || !user_info.image || !user_info.image.url) return;
-    $.get(user_info.image.url, onImageFetched);
-    // var xhr = new XMLHttpRequest();
-    // xhr.open('GET', user_info.image.url, true);
-    // xhr.responseType = 'blob';
-    // xhr.onload = onImageFetched;
-    // xhr.send();
-  }
-
-  function onImageFetched(e) {
-    if (this.status != 200) return;
-    var imgElem = document.createElement('img');
-    var objUrl = window.webkitURL.createObjectURL(this.response);
-    imgElem.src = objUrl;
-    imgElem.onload = function() {
-      window.webkitURL.revokeObjectURL(objUrl);
-    }
-    labelWelcome.insertAdjacentElement('afterbegin', imgElem);
+    if (!user_info) return;
+    labelWelcome.text('Hello, ' + user_info.displayName + '!');
+    if (!user_info.image || !user_info.image.url) return;
+    $(document.createElement('img'))
+      .attr('src', user_info.image.url)
+      .appendTo(labelWelcome);
   }
 
   // OnClick event handlers for the buttons.
-  function interactiveSignIn() {
+  function interactiveSignIn(callback) {
     changeState(STATE_ACQUIRING_AUTHTOKEN);
 
     chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
       if (chrome.runtime.lastError) {
-        sampleSupport.log(chrome.runtime.lastError);
+        console.log(chrome.runtime.lastError);
+        alert('Could not authenticate you. :(');
         changeState(STATE_START);
       } else {
-        sampleSupport.log('Token acquired:'+token+
+        console.log('Token acquired: ' + token +
           '. See chrome://identity-internals for details.');
         changeState(STATE_AUTHTOKEN_ACQUIRED);
+        callback();
       }
     });
   }
@@ -150,27 +161,27 @@ var googlePlusUserLoader = (function() {
 
           // Make a request to revoke token in the server
           $.get('https://accounts.google.com/o/oauth2/revoke?token=' + current_token);
-          // var xhr = new XMLHttpRequest();
-          // xhr.open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=' +
-          //          current_token);
-          // xhr.send();
 
           // Update the user interface accordingly
           changeState(STATE_START);
-          sampleSupport.log('Token revoked and removed from cache. '+
+          console.log('Token revoked and removed from cache. '+
             'Check chrome://identity-internals to confirm.');
         }
     });
   }
 
-
+  // Get all relevant information
+  function getData() {
+    getUserInfo();
+    getEvents();
+  }
 
   return {
     onload: function() {
       buttonSignin = $('button.signin');
-      buttonSignin.click(function() { getUserInfo(true); });
+      buttonSignin.click(function() { interactiveSignIn(getData); });
       labelWelcome = $('.welcome');
-      getUserInfo(false);
+      getData();
     }
   };
 
