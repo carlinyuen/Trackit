2fb21d15b1cfb48ab298fcd5577aa4ea15ae3cfb
Carlin Yuen <email.me@carlinyuen.com>
2018-01-30 15:05:39 -0500

    changing storage of artifact type and project to data-attributes

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -22,23 +22,30 @@ jQuery.noConflict();
     , ENUM_CAPITALIZATION_FIRST = 1
     , ENUM_CAPITALIZATION_ALL = 2
 
-    , EVENT_NAME_KEYPRESS = 'keypress.trackit'
-    , EVENT_NAME_KEYDOWN = 'keydown.trackit'
-    , EVENT_NAME_KEYUP = 'keyup.trackit'
-    , EVENT_NAME_BLUR = 'blur.trackit'
-    , EVENT_NAME_CLICK = 'click.trackit'
-    , EVENT_NAME_FOCUS = 'focus.trackit'
-    , EVENT_NAME_LOAD = 'load.trackit'
+    , NAMESPACE = 'trackit'
+    , EVENT_NAME_KEYPRESS = 'keypress.' + NAMESPACE
+    , EVENT_NAME_KEYDOWN = 'keydown.' + NAMESPACE
+    , EVENT_NAME_KEYUP = 'keyup.' + NAMESPACE
+    , EVENT_NAME_BLUR = 'blur.' + NAMESPACE
+    , EVENT_NAME_CLICK = 'click.' + NAMESPACE
+    , EVENT_NAME_FOCUS = 'focus.' + NAMESPACE
+    , EVENT_NAME_LOAD = 'load.' + NAMESPACE
     , EVENT_NAME_INSERTED = 'DOMNodeInserted'
 
     , SPOTLIGHT_INPUT = '*[contenteditable=true],textarea,input'
     , SPOTLIGHT_SHORTCUT = 'ctrl+space'
-    , SPOTLIGHT_CLASS = 'trackit-spotlight'
-    , SPOTLIGHT_SELECTOR = '.' + SPOTLIGHT_CLASS
-    , SPOTLIGHT_INPUT_CLASS = 'trackit-spotlight-input'
+    , SPOTLIGHT_ID = NAMESPACE + '-spotlight'
+    , SPOTLIGHT_SELECTOR = '#' + SPOTLIGHT_ID
+    , SPOTLIGHT_INPUT_CLASS = NAMESPACE + '-spotlight-input'
     , SPOTLIGHT_INPUT_SELECTOR = '.' + SPOTLIGHT_INPUT_CLASS
-    , SPOTLIGHT_INPUT_AI_CLASS = 'actionitem'
-    , SPOTLIGHT_INPUT_DC_CLASS = 'decision'
+
+    , SPOTLIGHT_PROJECT_DATA_ATTR = 'data-project'
+    , SPOTLIGHT_PROJECT_C = 'Collaboration'
+    , SPOTLIGHT_PROJECT_E = 'Engage'
+    , SPOTLIGHT_PROJECT_H = 'Huddle'
+    , SPOTLIGHT_TYPE_DATA_ATTR = 'data-type'
+    , SPOTLIGHT_TYPE_A = 'actionitem'
+    , SPOTLIGHT_TYPE_D = 'decision'
   ;
 
   var typingBuffer = [];		// Keep track of what's been typed before timeout
@@ -61,7 +68,7 @@ jQuery.noConflict();
 
     // Check if there's already a spotlight bar, and if so, just focus
     if ($(SPOTLIGHT_INPUT_SELECTOR).length > 0) {
-      $(SPOTLIGHT_INPUT_SELECTOR).focus();
+      $(SPOTLIGHT_INPUT_SELECTOR).focus().fadeIn(ANIMATION_FAST);
     } else {
       addSpotlightBar('body');
     }
@@ -72,7 +79,7 @@ jQuery.noConflict();
   function addSpotlightBar(elementSelector) {
     var d = document;
     $(d.createElement('form'))
-      .addClass(SPOTLIGHT_CLASS)
+      .attr('id', SPOTLIGHT_ID)
       .append($(d.createElement('input'))
         .addClass(SPOTLIGHT_INPUT_CLASS)
         .attr('type', 'text')
@@ -90,8 +97,6 @@ jQuery.noConflict();
   // Hide the spotlight bar
   function hideSpotlight(callback) {
     $(SPOTLIGHT_SELECTOR).fadeOut(ANIMATION_FAST, function() {
-      $(SPOTLIGHT_SELECTOR).remove();
-
       // Call callback if it is a function
       if (callback && typeof(callback) === 'function') {
         callback();
@@ -128,6 +133,11 @@ jQuery.noConflict();
   // When user lifts up on a key, to catch backspace
   function keyUpHandler(event)
   {
+    // Clear field if empty
+    if (event.target.value === ' ') {
+      event.target.value = '';
+    }
+
     // Make sure it's not the same event firing over and over again
     if (keyUpEvent == event) {
       return;
@@ -141,10 +151,20 @@ jQuery.noConflict();
     // When user types backspace, pop character off buffer
     if (charCode == KEYCODE_BACKSPACE) {
       typingBuffer.pop(); // Remove last character typed
+
+      // Clear data type if backspacing on empty field
+      if (event.target.value === '') {
+        var $target = $(event.target);
+        if ($target.attr(SPOTLIGHT_TYPE_DATA_ATTR)) {
+          $target.removeAttr(SPOTLIGHT_TYPE_DATA_ATTR);
+        } else if ($target.attr(SPOTLIGHT_PROJECT_DATA_ATTR)) {
+          $target.removeAttr(SPOTLIGHT_PROJECT_DATA_ATTR);
+        }
+      }
     }
 
     // If user uses tab or return, clear and get out
-    if (charCode == KEYCODE_TAB || charCode == KEYCODE_RETURN || event.target.value === "") {
+    if (charCode == KEYCODE_TAB || charCode == KEYCODE_RETURN || event.target.value === '') {
       return clearTypingBuffer();
     }
   }
@@ -164,16 +184,40 @@ jQuery.noConflict();
     shortcut = shortcut.toUpperCase();
 
     switch (shortcut) {
-      case 'AI: ':
-        $spotlight.removeClass(SPOTLIGHT_INPUT_DC_CLASS)
-          .addClass(SPOTLIGHT_INPUT_AI_CLASS);
+
+      // Action item
+      case 'A: ':
+        $spotlight.attr(SPOTLIGHT_TYPE_DATA_ATTR, SPOTLIGHT_TYPE_A);
         $(textInput).val('');
         break;
-      case 'DC: ':
-        $spotlight.removeClass(SPOTLIGHT_INPUT_AI_CLASS)
-          .addClass(SPOTLIGHT_INPUT_DC_CLASS);
+
+      // Decision
+      case 'D: ':
+        $spotlight.attr(SPOTLIGHT_TYPE_DATA_ATTR, SPOTLIGHT_TYPE_D);
         $(textInput).val('');
         break;
+
+      // Project
+      case '#ENGAGE ':
+        $spotlight.attr(SPOTLIGHT_PROJECT_DATA_ATTR, SPOTLIGHT_PROJECT_E);
+        processAutoTextExpansion(shortcut,
+                    data[shortcutKeyLowercase],
+                    lastChar,
+                    textInput,
+                    (isAllCaps ? ENUM_CAPITALIZATION_ALL : ENUM_CAPITALIZATION_FIRST)
+                  );
+        break;
+
+      // Project
+      case '#COLLAB ':
+        $spotlight.attr(SPOTLIGHT_PROJECT_DATA_ATTR, SPOTLIGHT_PROJECT_C);
+        break;
+
+      // Project
+      case '#HUDDLE ':
+        $spotlight.attr(SPOTLIGHT_PROJECT_DATA_ATTR, SPOTLIGHT_PROJECT_H);
+        break;
+
       default:
         // do nothing
     }
@@ -236,183 +280,183 @@ jQuery.noConflict();
   //     }
   //   });
   // }
-  //
-  // // Process autotext expansion and replace text
-  // function processAutoTextExpansion(shortcut, autotext, lastChar, textInput, capitalization)
-  // {
-  //   debugLog('processAutoTextExpansion:', autotext, capitalization);
-  //
-  //   // Check if shortcut exists and should be triggered
-  //   if (autotext && textInput)
-  //   {
-  //     // If shortcuts are disabled, abort early
-  //     if (disableShortcuts) {
-  //       return;
-  //     }
-  //
-  //     // Update / get clipboard text
-  //     getClipboardData(function()
-  //     {
-  //       // Handle clipboard pastes
-  //       autotext = processClips(autotext);
-  //
-  //       // Handle moment.js dates
-  //       autotext = processDates(autotext);
-  //
-  //       // Handle %url% macro
-  //       autotext = processUrls(autotext);
-  //
-  //       // Adjust capitalization
-  //       switch (capitalization)
-  //       {
-  //         case ENUM_CAPITALIZATION_FIRST:
-  //           autotext = autotext.charAt(0).toUpperCase() + autotext.slice(1);
-  //           break;
-  //
-  //         case ENUM_CAPITALIZATION_ALL:
-  //           autotext = autotext.toUpperCase();
-  //           break;
-  //
-  //         default: break;
-  //       }
-  //
-  //       // Setup for processing
-  //       var domain = window.location.host;
-  //       debugLog('textInput: ', textInput);
-  //
-  //       // If input or textarea field, can easily change the val
-  //       if (textInput.nodeName == 'TEXTAREA' || textInput.nodeName == 'INPUT')
-  //       {
-  //         // Add whitespace if was last character
-  //         if (WHITESPACE_REGEX.test(lastChar)) {
-  //           autotext += lastChar;
-  //         }
-  //
-  //         replaceTextRegular(shortcut, autotext, textInput);
-  //       }
-  //       else	// Trouble... editable divs & special cases
-  //       {
-  //         // Add whitespace if was last character
-  //         if (lastChar == ' ') {
-  //           autotext += '&nbsp;';
-  //         } else if (lastChar == '\t') {
-  //           autoText += '&#9;';
-  //         }
-  //
-  //         debugLog('Domain:', domain);
-  //         replaceTextContentEditable(shortcut, autotext, findFocusedNode());
-  //       }
-  //
-  //       // Always clear the buffer after a shortcut fires
-  //       clearTypingBuffer();
-  //     });	// END - getClipboardData()
-  //   }	// END - if (autotext)
-  //   else {  // Error
-  //     console.log('Invalid input, missing autotext or textinput parameters.');
-  //   }
-  // }
-  //
-  // // Specific handler for regular textarea and input elements
-  // function replaceTextRegular(shortcut, autotext, textInput)
-  // {
-  //   var cursorPosition = getCursorPosition(textInput);
-  //
-  //   // Fix for input[type=email] and input[type=number]
-  //   if (cursorPosition === 0 && textInput.nodeName == 'INPUT')
-  //   {
-  //     var type = textInput.getAttribute('type').toUpperCase();
-  //     if (type == 'EMAIL' || type == 'NUMBER') {
-  //       cursorPosition = textInput.value.length;
-  //     }
-  //   }
-  //
-  //   textInput.value = replaceText(
-  //     textInput.value,
-  //     shortcut,
-  //     autotext,
-  //     cursorPosition
-  //   );
-  //   setCursorPosition(textInput, cursorPosition - shortcut.length + autotext.length);
-  // }
-  //
-  // // Reusable handler for editable iframe text replacements
-  // function replaceTextContentEditable(shortcut, autotext, node, win)
-  // {
-  //   // Find focused div instead of what's receiving events
-  //   var textInput = node.parentNode;
-  //   debugLog(textInput);
-  //
-  //   // Get and process text, update cursor position
-  //   var cursorPosition = getCursorPosition(textInput, win)
-  //     , text = replaceHTML(node.textContent, shortcut, autotext, cursorPosition)
-  //     , multiline = false
-  //     , lines
-  //   ;
-  //
-  //   // If autotext is multiline text, split by newlines, join with <br> tag instead
-  //   if (autotext.indexOf('\n') >= 0)
-  //   {
-  //     lines = text.split('\n');
-  //     text = lines.join('<br>');
-  //     multiline = true;
-  //   }
-  //
-  //   // A way to insert HTML into a content editable div with raw JS.
-  //   //  Creates an element with the HTML content, then transfers node by node
-  //   //  to a new Document Fragment that replaces old node
-  //   //  Source from: http://stackoverflow.com/questions/6690752/insert-html-at-caret-in-a-contenteditable-div
-  //   var el = document.createElement('div')          // Used to store HTML
-  //     , frag = document.createDocumentFragment()  // To replace old node
-  //     , cursorNode;                               // To track cursor position
-  //   el.innerHTML = text;                            // Set HTML to div, then move to frag
-  //   for (var tempNode; tempNode = el.firstChild; frag.appendChild(tempNode))
-  //   {
-  //     debugLog(tempNode.nodeType, tempNode);
-  //     if (tempNode.nodeType === Node.COMMENT_NODE
-  //       && tempNode.nodeValue == CURSOR_TRACKING_TAG) {
-  //       cursorNode = tempNode;
-  //     }
-  //   }
-  //   textInput.replaceChild(frag, node);             // Replace old node with frag
-  //
-  //   // Set cursor position based off tracking node (or last child if we
-  //   //  weren't able to find the cursor tracker), then remove tracking node
-  //   setCursorPositionAfterNode(cursorNode || textInput.lastChild, win);
-  //   if (cursorNode) {
-  //     cursorNode.parentNode.removeChild(cursorNode);
-  //   }
-  // }
-  //
-  // // Replacing shortcut with autotext in text at cursorPosition
-  // function replaceText(text, shortcut, autotext, cursorPosition)
-  // {
-  //   debugLog('cursorPosition:', cursorPosition);
-  //   debugLog('currentText:', text);
-  //   debugLog('shortcut:', shortcut);
-  //   debugLog('expandedText:', autotext);
-  //
-  //   // Replace shortcut based off cursorPosition
-  //   return [text.slice(0, cursorPosition - shortcut.length),
-  //     autotext, text.slice(cursorPosition)].join('');
-  // }
-  //
-  // // Replacing shortcut with autotext HTML content at cursorPosition
-  // function replaceHTML(text, shortcut, autotext, cursorPosition)
-  // {
-  //   debugLog('cursorPosition:', cursorPosition);
-  //   debugLog('currentText:', text);
-  //   debugLog('shortcut:', shortcut);
-  //   debugLog('expandedText:', autotext);
-  //
-  //   // If autotext expansion already has cursor tag in it, don't insert
-  //   var cursorTag = (autotext.indexOf(CURSOR_TRACKING_HTML) >= 0)
-  //     ? '' : CURSOR_TRACKING_HTML;
-  //
-  //   // Replace shortcut based off cursorPosition,
-  //   //  insert tracking tag for cursor if it isn't already defined in autotext
-  //   return [text.slice(0, cursorPosition - shortcut.length),
-  //     autotext, cursorTag, text.slice(cursorPosition)].join('');
-  // }
+
+  // Process autotext expansion and replace text
+  function processAutoTextExpansion(shortcut, autotext, lastChar, textInput, capitalization)
+  {
+    debugLog('processAutoTextExpansion:', autotext, capitalization);
+
+    // Check if shortcut exists and should be triggered
+    if (autotext && textInput)
+    {
+      // If shortcuts are disabled, abort early
+      if (disableShortcuts) {
+        return;
+      }
+
+      // Update / get clipboard text
+      getClipboardData(function()
+      {
+        // // Handle clipboard pastes
+        // autotext = processClips(autotext);
+        //
+        // // Handle moment.js dates
+        // autotext = processDates(autotext);
+        //
+        // // Handle %url% macro
+        // autotext = processUrls(autotext);
+
+        // Adjust capitalization
+        switch (capitalization)
+        {
+          case ENUM_CAPITALIZATION_FIRST:
+            autotext = autotext.charAt(0).toUpperCase() + autotext.slice(1);
+            break;
+
+          case ENUM_CAPITALIZATION_ALL:
+            autotext = autotext.toUpperCase();
+            break;
+
+          default: break;
+        }
+
+        // Setup for processing
+        var domain = window.location.host;
+        debugLog('textInput: ', textInput);
+
+        // If input or textarea field, can easily change the val
+        if (textInput.nodeName == 'TEXTAREA' || textInput.nodeName == 'INPUT')
+        {
+          // Add whitespace if was last character
+          if (WHITESPACE_REGEX.test(lastChar)) {
+            autotext += lastChar;
+          }
+
+          replaceTextRegular(shortcut, autotext, textInput);
+        }
+        else	// Trouble... editable divs & special cases
+        {
+          // Add whitespace if was last character
+          if (lastChar == ' ') {
+            autotext += '&nbsp;';
+          } else if (lastChar == '\t') {
+            autoText += '&#9;';
+          }
+
+          debugLog('Domain:', domain);
+          replaceTextContentEditable(shortcut, autotext, findFocusedNode());
+        }
+
+        // Always clear the buffer after a shortcut fires
+        clearTypingBuffer();
+      });	// END - getClipboardData()
+    }	// END - if (autotext)
+    else {  // Error
+      console.log('Invalid input, missing autotext or textinput parameters.');
+    }
+  }
+
+  // Specific handler for regular textarea and input elements
+  function replaceTextRegular(shortcut, autotext, textInput)
+  {
+    var cursorPosition = getCursorPosition(textInput);
+
+    // Fix for input[type=email] and input[type=number]
+    if (cursorPosition === 0 && textInput.nodeName == 'INPUT')
+    {
+      var type = textInput.getAttribute('type').toUpperCase();
+      if (type == 'EMAIL' || type == 'NUMBER') {
+        cursorPosition = textInput.value.length;
+      }
+    }
+
+    textInput.value = replaceText(
+      textInput.value,
+      shortcut,
+      autotext,
+      cursorPosition
+    );
+    setCursorPosition(textInput, cursorPosition - shortcut.length + autotext.length);
+  }
+
+  // Reusable handler for editable iframe text replacements
+  function replaceTextContentEditable(shortcut, autotext, node, win)
+  {
+    // Find focused div instead of what's receiving events
+    var textInput = node.parentNode;
+    debugLog(textInput);
+
+    // Get and process text, update cursor position
+    var cursorPosition = getCursorPosition(textInput, win)
+      , text = replaceHTML(node.textContent, shortcut, autotext, cursorPosition)
+      , multiline = false
+      , lines
+    ;
+
+    // If autotext is multiline text, split by newlines, join with <br> tag instead
+    if (autotext.indexOf('\n') >= 0)
+    {
+      lines = text.split('\n');
+      text = lines.join('<br>');
+      multiline = true;
+    }
+
+    // A way to insert HTML into a content editable div with raw JS.
+    //  Creates an element with the HTML content, then transfers node by node
+    //  to a new Document Fragment that replaces old node
+    //  Source from: http://stackoverflow.com/questions/6690752/insert-html-at-caret-in-a-contenteditable-div
+    var el = document.createElement('div')          // Used to store HTML
+      , frag = document.createDocumentFragment()  // To replace old node
+      , cursorNode;                               // To track cursor position
+    el.innerHTML = text;                            // Set HTML to div, then move to frag
+    for (var tempNode; tempNode = el.firstChild; frag.appendChild(tempNode))
+    {
+      debugLog(tempNode.nodeType, tempNode);
+      if (tempNode.nodeType === Node.COMMENT_NODE
+        && tempNode.nodeValue == CURSOR_TRACKING_TAG) {
+        cursorNode = tempNode;
+      }
+    }
+    textInput.replaceChild(frag, node);             // Replace old node with frag
+
+    // Set cursor position based off tracking node (or last child if we
+    //  weren't able to find the cursor tracker), then remove tracking node
+    setCursorPositionAfterNode(cursorNode || textInput.lastChild, win);
+    if (cursorNode) {
+      cursorNode.parentNode.removeChild(cursorNode);
+    }
+  }
+
+  // Replacing shortcut with autotext in text at cursorPosition
+  function replaceText(text, shortcut, autotext, cursorPosition)
+  {
+    debugLog('cursorPosition:', cursorPosition);
+    debugLog('currentText:', text);
+    debugLog('shortcut:', shortcut);
+    debugLog('expandedText:', autotext);
+
+    // Replace shortcut based off cursorPosition
+    return [text.slice(0, cursorPosition - shortcut.length),
+      autotext, text.slice(cursorPosition)].join('');
+  }
+
+  // Replacing shortcut with autotext HTML content at cursorPosition
+  function replaceHTML(text, shortcut, autotext, cursorPosition)
+  {
+    debugLog('cursorPosition:', cursorPosition);
+    debugLog('currentText:', text);
+    debugLog('shortcut:', shortcut);
+    debugLog('expandedText:', autotext);
+
+    // If autotext expansion already has cursor tag in it, don't insert
+    var cursorTag = (autotext.indexOf(CURSOR_TRACKING_HTML) >= 0)
+      ? '' : CURSOR_TRACKING_HTML;
+
+    // Replace shortcut based off cursorPosition,
+    //  insert tracking tag for cursor if it isn't already defined in autotext
+    return [text.slice(0, cursorPosition - shortcut.length),
+      autotext, cursorTag, text.slice(cursorPosition)].join('');
+  }
 
     // Find node that has text contents that matches text
   function findMatchingTextNode(div, text)
@@ -604,6 +648,14 @@ jQuery.noConflict();
       range.setStart(node, pos);
       range.select();
     }
+  }
+
+  // Remove classes that start with a prefix
+  //  param: $target should be a jQuery object
+  function removeClassesStartingWith($target, prefix) {
+    $target.removeClass(function (index, className) {
+      return (className.match(new RegExp('/(^|\s)' + prefix + '\S+/g')) || []).join(' ');
+    });
   }
 
   // // Process and replace %url% tags with content from current url
